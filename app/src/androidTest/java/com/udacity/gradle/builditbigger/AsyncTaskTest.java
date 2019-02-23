175bfcb70d044e01b14a8d995e61b4cbd4297620
Charles Rowland <charlesrowland@Charless-MacBook-Pro.local>
2019-02-23 14:03:48 -0700

    added asyncTastTest, even though the backend refuses to let things run

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -1,12 +1,28 @@
 package com.udacity.gradle.builditbigger;
 
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class AsyncTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    @Test
+    public void testAsyncTask() throws InterruptedException, ExecutionException {
+        EndpointAsyncTask endpointAsyncTask = new EndpointAsyncTask();
+        endpointAsyncTask.execute(mainActivityActivityTestRule.getActivity());
 
+        String joke = endpointAsyncTask.get();
+        assertNotNull(joke);
+    }
 }
